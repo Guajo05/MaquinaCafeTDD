d1e536b849b0c6148072dd31ea5d4cd0a46ba269
Darbin Luis <darbinluis05@gmail.com>
2026-06-23 22:00:16 -0400

    Agregar archivos de proyecto.

---
+++ MaquinaCafe.Tests/MaquinaCafeTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MaquinaCafe.Tests
+{
+    [TestFixture]
+    public class MaquinaCafeTests
+    {
+        // TC-01
+        [Test]
+        public void InsertarMoneda_MontoValido_IncrementaSaldo()
+        {
+            var maquina = new MaquinaCafe();
+            maquina.InsertarMoneda(25);
+            Assert.AreEqual(25, maquina.Saldo);
+        }
+
+        // TC-02
+        [Test]
+        public void SeleccionarBebida_SaldoSuficiente_RetornaTrue()
+        {
+            var maquina = new MaquinaCafe();
+            maquina.InsertarMoneda(100);
+            var resultado = maquina.SeleccionarBebida("Cafe");
+            Assert.IsTrue(resultado);
+        }
+
+        // TC-03
+        [Test]
+        public void SeleccionarBebida_SaldoInsuficiente_RetornaFalse()
+        {
+            var maquina = new MaquinaCafe();
+            maquina.InsertarMoneda(50);
+            var resultado = maquina.SeleccionarBebida("Cafe");
+            Assert.IsFalse(resultado);
+        }
+
+        // TC-04
+        [Test]
+        public void ObtenerCambio_DespuesDeCompra_DevuelveSaldoRestante()
+        {
+            var maquina = new MaquinaCafe();
+            maquina.InsertarMoneda(150);
+            maquina.SeleccionarBebida("Cafe");
+            var cambio = maquina.ObtenerCambio();
+            Assert.AreEqual(50, cambio);
+            Assert.AreEqual(0, maquina.Saldo);
+        }
+
+        // TC-05
+        [Test]
+        public void SeleccionarBebida_BebidaInexistente_LanzaArgumentException()
+        {
+            var maquina = new MaquinaCafe();
+            Assert.Throws<ArgumentException>(() => maquina.SeleccionarBebida("Jugo"));
+        }
+
+        // TC-06
+        [Test]
+        public void ObtenerMenu_RetornaDiccionarioConTresBebidas()
+        {
+            var maquina = new MaquinaCafe();
+            var menu = maquina.ObtenerMenu();
+            Assert.AreEqual(3, menu.Count);
+            Assert.AreEqual(100, menu["Cafe"]);
+            Assert.AreEqual(75, menu["Te"]);
+            Assert.AreEqual(50, menu["Agua"]);
+        }
+
+        // TC-07
+        [Test]
+        public void DevolverMonedas_ReiniciaSaldoACero()
+        {
+            var maquina = new MaquinaCafe();
+            maquina.InsertarMoneda(100);
+            maquina.DevolverMonedas();
+            Assert.AreEqual(0, maquina.Saldo);
+        }
+
+        // TC-08 (Este fallará hasta que implementemos stock en el Refactor)
+        [Test]
+        public void SeleccionarBebida_StockAgotado_RetornaFalse()
+        {
+            var maquina = new MaquinaCafe();
+            maquina.InsertarMoneda(1100); // Para 11 cafés
+            for (int i = 0; i < 10; i++) maquina.SeleccionarBebida("Cafe");
+            var resultado = maquina.SeleccionarBebida("Cafe");
+            Assert.IsFalse(resultado);
+        }
+    }
+}
---
+++ MaquinaCafe/MaquinaCafe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaquinaCafe
+{
+    public class MaquinaCafe
+    {
+        // Propiedad solo para que compile, siempre devuelve 0
+        public int Saldo => 0;
+
+        // Método vacío (no hace nada)
+        public void InsertarMoneda(int monto) { }
+
+        // Siempre devuelve false (para que fallen los tests de selección exitosa)
+        public bool SeleccionarBebida(string bebida)
+        {
+            // Lanzamos excepción para forzar que el TC-05 (bebida inexistente) falle también
+            // o simplemente devolvemos false. Como necesitamos que falle TC-02, devolvemos false.
+            return false;
+        }
+
+        // Siempre devuelve 0
+        public int ObtenerCambio() => 0;
+
+        // Vacío
+        public void DevolverMonedas() { }
+
+        // Devuelve un diccionario vacío (para que TC-06 falle al contar 0 vs 3)
+        public Dictionary<string, int> ObtenerMenu() => new Dictionary<string, int>();
+    }
+}
