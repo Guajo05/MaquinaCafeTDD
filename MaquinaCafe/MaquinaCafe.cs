72a84ee788c9634d3291454f1026743aa3ca17b8
Darbin Luis <darbinluis05@gmail.com>
2026-06-23 22:16:40 -0400

    GREEN - Implementación mínima para pasar TC-01 a TC-07 (stock pendiente)

---
+++ MaquinaCafe/MaquinaCafe.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaquinaCafe
+{
+    public class MaquinaCafe
+    {
+        public int Saldo { get; private set; }
+
+        private readonly Dictionary<string, int> _menu = new()
+        {
+            { "Cafe", 100 },
+            { "Te", 75 },
+            { "Agua", 50 }
+        };
+
+        public void InsertarMoneda(int monto) => Saldo += monto;
+
+        public bool SeleccionarBebida(string bebida)
+        {
+            if (!_menu.ContainsKey(bebida))
+                throw new ArgumentException("No existe");
+
+            if (Saldo < _menu[bebida])
+                return false;
+
+            Saldo -= _menu[bebida];
+            return true;
+        }
+
+        public int ObtenerCambio()
+        {
+            var cambio = Saldo;
+            Saldo = 0;
+            return cambio;
+        }
+
+        public void DevolverMonedas() => Saldo = 0;
+
+        public Dictionary<string, int> ObtenerMenu() => _menu;
+    }
+}
